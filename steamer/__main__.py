3d24f028f423d413c79e85caa91cab0eadf0b442
John Carlson <johncarlson@protonmail.com>
2022-06-27 23:29:08 -0400

    main: Add addshortcut and downloadartwork funcs

--- steamer/__main__.py
+++ steamer/__main__.py
@@ -1,5 +1,22 @@
+from . import shortcuts
+from . import artwork
 import sys
 import argparse
+import os
+
+
+def add_shortcut(args):
+    shortcuts_file = shortcuts.load_steam_shortcuts()
+    full_path = os.path.abspath(args.target)
+    new_shortcut = shortcuts.SteamShortcut(args.name,
+                                           full_path,
+                                           os.getcwd(),
+                                           args.directory)
+    shortcuts_file.add_shortcut(new_shortcut)
+
+
+def download_artwork(args):
+    artwork.download_assets_for_steamid(args.appid)
 
 
 parser = argparse.ArgumentParser(prog='steamer',
@@ -8,8 +25,12 @@ subparses = parser.add_subparsers()
 shortcut_parser = subparses.add_parser('addshortcut')
 shortcut_parser.add_argument('name', help='Name for the shortcut')
 shortcut_parser.add_argument('target', help='Path to program/game to run')
+shortcut_parser.set_defaults(func=add_shortcut)
 
-shortcut_parser = subparses.add_parser('downloadartwork')
-shortcut_parser.add_argument('appid', help='Steam appid to download artwork from')
+download_parser = subparses.add_parser('downloadartwork')
+download_parser.add_argument('appid', help='Steam appid to download artwork from')
+download_parser.set_defaults(func=download_artwork)
 
-parser.parse_args()
+args = parser.parse_args()
+
+
